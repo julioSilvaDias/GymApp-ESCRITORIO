9e37f26769a9ed458fe55d8a65f568e7b4b75822
in2dm3-v <in2dm3-v@elorrieta.int>
2024-10-23 19:23:05 +0200

    Pojo Ejercicio modificado

--- GymApp/src/main/java/gymApp/bbdd/pojos/Ejercicio.java
+++ GymApp/src/main/java/gymApp/bbdd/pojos/Ejercicio.java
@@ -1,30 +1,15 @@
 package gymApp.bbdd.pojos;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Ejercicio extends Workout{
 	String description;
 	String image;
 	String name;
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + Objects.hash(description, image, name);
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Ejercicio other = (Ejercicio) obj;
-		return Objects.equals(description, other.description) && Objects.equals(image, other.image)
-				&& Objects.equals(name, other.name);
-	}
+	int rest;
+	List<Integer> sets;
+	
 	public String getDescription() {
 		return description;
 	}
@@ -43,10 +28,41 @@ public class Ejercicio extends Workout{
 	public void setName(String name) {
 		this.name = name;
 	}
+	public int getRest() {
+		return rest;
+	}
+	public void setRest(int rest) {
+		this.rest = rest;
+	}
+	public List<Integer> getSets() {
+		return sets;
+	}
+	public void setSets(List<Integer> sets) {
+		this.sets = sets;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(description, image, name, rest, sets);
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ejercicio other = (Ejercicio) obj;
+		return Objects.equals(description, other.description) && Objects.equals(image, other.image)
+				&& Objects.equals(name, other.name) && rest == other.rest && Objects.equals(sets, other.sets);
+	}
 	@Override
 	public String toString() {
-		return "Ejercicio [description=" + description + ", image=" + image + ", name=" + name + ", video=" + video
-				+ ", exercises=" + exercises + ", nivel=" + nivel + "]";
+		return "Ejercicio [description=" + description + ", image=" + image + ", name=" + name + ", rest=" + rest
+				+ ", sets=" + sets + "]";
 	}
 	
 	
